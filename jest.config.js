dc3432ca02ea5d4f2876589c9c7804ee3f07d7c3
Remulo Costa <remulo.costa@gmail.com>
2020-11-29 19:22:20 -0300

    chore: fixup add react config

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     '<rootDir>/src/**/*.{ts,tsx}'
   ],
   coverageDirectory: 'coverage',
-  testEnvironment: 'jsnode',
+  testEnvironment: 'jsdom',
   transform: {
     '.+\\.(ts|tsx)$': 'ts-jest'
   },
